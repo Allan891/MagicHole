571cd1c3d081a2fe15dddd4ff3846e5884df2abf
f-r00t <fruktstav@gmail.com>
2025-03-27 12:09:17 +0100

    Added correct icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,6 +8,9 @@ import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+import { MaterialIcons } from '@expo/vector-icons'; // Import directly
+
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
@@ -29,22 +32,22 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Play',
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="golf-course" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Scoreboard',
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="sports-score" color={color} />, // Change from "paperplane.fill"
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="settings" color={color} />,
         }}
       />
     </Tabs>
