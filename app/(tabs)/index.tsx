0336ba8f39039072baf4f0217bf23dc2779b8714
plxxl2 <andreas@modahl.se>
2025-03-28 16:10:25 +0100

    Update index.tsx
    index now redirects to map.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,51 +1,6 @@
-import {
-  View,
-  StyleSheet,
-  Dimensions,
-  TouchableOpacity,
-  Animated,
-  PanResponder,
-  TextInput,
-  Button,
-  Alert,
-} from 'react-native';
-export default function HomeScreen() {
-  //return <Redirect href="(tabs)/map" />;
-}
+import { Redirect } from "expo-router";
 
-const styles = StyleSheet.create({
-  bottomSheet: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    height: Dimensions.get('window').height,
-    backgroundColor: 'white',
-    borderTopLeftRadius: 16,
-    borderTopRightRadius: 16,
-    padding: 16,
-    zIndex: 999,
-  },
-  sheetHandle: {
-    width: 50,
-    height: 5,
-    borderRadius: 2.5,
-    backgroundColor: '#ccc',
-    alignSelf: 'center',
-    marginBottom: 10,
-  },
-  sheetTitle: {
-    fontWeight: 'bold',
-    fontSize: 16,
-    marginBottom: 10,
-    textAlign: 'center',
-  },
-  input: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 6,
-    paddingHorizontal: 10,
-    textAlign: 'center',
-    marginBottom: 12,
-  },
-});
+const Index = () => {
+  return <Redirect href="/map" />;
+};
+export default Index;
