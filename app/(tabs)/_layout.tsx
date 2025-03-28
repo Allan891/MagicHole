e4e281d42366bf157f70f361236fc2c9efa05cef
plxxl2 <andreas@modahl.se>
2025-03-28 16:08:39 +0100

    New Index file (because it is the default first navigation
    Index tab is hidden in the "tab bar"

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,14 +3,25 @@ import { RoundProvider } from './RoundContext';
 import { ThemeProvider, DefaultTheme } from '@react-navigation/native';
 import { MaterialIcons } from '@expo/vector-icons'; // Import directly
 
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { useClientOnlyValue } from "@/hooks/useClientOnlyValue";
 
 
 export default function TabLayout() {
   return (
     <RoundProvider>
       <ThemeProvider value={DefaultTheme}>
-        <Tabs>
+
+        <Tabs initialRouteName="map">
+        <Tabs.Screen name="index"
+                    options={{
+                      tabBarItemStyle: { title: 'no' ,display: "none" },
+                    }}
+                  />
         <Tabs.Screen name="map" options={{ title: 'Map',  tabBarIcon: ({ color }) => <MaterialIcons size={28} name="golf-course" color={color} />, }} />
         <Tabs.Screen name="score" options={{ title: 'Scorecard', tabBarIcon: ({ color }) => <MaterialIcons size={28} name="sports-score" color={color} />, // Change from "paperplane.fill"
  }} />
