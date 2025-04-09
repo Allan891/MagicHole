78f91580be2f37ec192821aec56850ec959d67b1
plxxl2 <andreas@modahl.se>
2025-04-09 14:39:09 +0200

    Update stats.ts
    commented out console log functions to clear up log.

--- utils/stats.ts
+++ utils/stats.ts
@@ -3,7 +3,7 @@ import {calculateBearing, calculateDistance} from '@/utils';
 export 
   const calculateStrokesGained = (StartSlag:Stroke , slutSlag:Stroke, latitude:number , longitude:number) =>{
     var rvalue = calculateRawSG(StartSlag,latitude,longitude) - 1 - calculateRawSG(slutSlag,latitude,longitude)
-    console.log("Strokes Gained: ", rvalue)
+    //console.log("Strokes Gained: ", rvalue)
     return rvalue
   }
   
@@ -25,7 +25,7 @@ export
     var sGValues
     //var name = "Fairway"
 
-    console.log('slag.lie ', slag.lie)
+    //console.log('slag.lie ', slag.lie)
     switch(slag.lie){
      case (0): {
          sGValues = sGdata.data.map(t=>t.Tee)
@@ -55,9 +55,9 @@ export
    // var sGValues = sGdata.data.map(t=>t.name)
 
     //sGdata.data[slag.lie]
-    console.log('SGVALUES', sGValues)
+    //console.log('SGVALUES', sGValues)
     const DLeft = calculateDistance(slag.startLatitude, slag.startLongitude,latitude,longitude)
-    console.log('sGDistance', sGDistance)
+    //console.log('sGDistance', sGDistance)
     var indices = findSurroundingIndices(sGDistance,DLeft)
     var firstSG = sGValues[indices[0]]
     var secondSG = sGValues[indices[1]]
@@ -68,7 +68,7 @@ export
 
 
         rvalue = firstSG + (DLeft - firstDistance)  / (secondDistance - firstDistance) * (secondSG - firstSG)
-        console.log('Raw Strokes Gained: ', rvalue)
+        //console.log('Raw Strokes Gained: ', rvalue)
         return rvalue
     }
     const sGDistanceGreen = sGdata.green["Distance (Meters)"]
@@ -84,19 +84,19 @@ export
     if (slag.lie = 4) // lie = 4 Equals "Green"
 
             rvalue = firstSGGreen + (DLeftGreen - firstDistanceGreen)  / (secondDistanceGreen - firstDistanceGreen) * (secondSGGreen - firstSGGreen)
-            console.log('Raw Strokes Gained: ', rvalue)
+            //console.log('Raw Strokes Gained: ', rvalue)
             return rvalue
 
  }
 
   const findSurroundingIndices = (distanceArray:number[], value:number) => {
-      console.log("asdf")
+      //console.log("asdf")
     for (let i = 0; i < distanceArray.length - 1; i++) {
       if (distanceArray[i] <= value && value < distanceArray[i + 1]) {
         return [i, i + 1];
       }
     }
-    console.log("Indices",distanceArray.length - 2, distanceArray.length - 1)
+    //console.log("Indices",distanceArray.length - 2, distanceArray.length - 1)
     return [distanceArray.length - 2, distanceArray.length - 1]; // fallback
   }
 
