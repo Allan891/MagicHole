3611620850c9339cb2dc2653188b01b8a7c2fc75
plxxl2 <andreas@modahl.se>
2025-04-10 12:47:57 +0200

    Update _layout.tsx
    Added Stats and hid summary tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,6 +26,12 @@ export default function TabLayout() {
         <Tabs.Screen name="score" options={{ title: 'Scorecard', tabBarIcon: ({ color }) => <MaterialIcons size={28} name="sports-score" color={color} />, // Change from "paperplane.fill"
  }} />
         <Tabs.Screen name="history" options={{headerShown: false, title: 'History',  tabBarIcon: ({ color }) => <MaterialIcons size={28} name="list" color={color} />, }} />
+        <Tabs.Screen name="stats" options={{ title: 'Stats', tabBarIcon: ({ color }) => <MaterialIcons size={28} name="query-stats" color={color} />, // Change from "paperplane.fill"
+              }} />
+
+         <Tabs.Screen name="summary" options={{ title: 'SummARY', headerShown: false,
+                                                                                        tabBarItemStyle: { title: 'no' ,display: "none" },tabBarIcon: ({ color }) => <MaterialIcons size={28} name="sports-score" color={color} />, // Change from "paperplane.fill"
+          }} />
         </Tabs>
     </ThemeProvider>
     
