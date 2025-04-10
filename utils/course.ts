cc6e241a2cc68014a6d06f86ee78d2ce858830fe
f-r00t <fruktstav@gmail.com>
2025-04-10 14:37:20 +0200

    Add util functions for courses

--- utils/course.ts
+++ utils/course.ts
@@ -1,7 +1,17 @@
 import courses from '@/constants/courses';
 
 export const getPar = (courseid) =>{
-    const dis = courses.find(a => a.id === courseid)?.par;
-    console.log("desto", dis);
-    return dis;
+    return courses.find(a => a.id === courseid)?.par;
   }
+
+export const getHole = (courseid, i) =>{
+  return courses.find(a => a.id === courseid)?.holes[i];
+}
+
+export const getHoles = (courseid) =>{
+  return courses.find(a => a.id === courseid)?.holes;
+}
+
+export const getCourse = (courseid) => {
+  return courses.find(a => a.id == courseid);
+}
