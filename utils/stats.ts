750b41a25b638a984f2642ced2ffb5756c2c0964
plxxl2 <andreas@modahl.se>
2025-04-10 12:48:55 +0200

    Update stats.ts

--- utils/stats.ts
+++ utils/stats.ts
@@ -1,12 +1,13 @@
-import {calculateBearing, calculateDistance} from '@/utils';
+import {calculateBearing, calculateDistance, getHole} from '@/utils';
+import db from '../app/db/db';
 
-export 
+export
   const calculateStrokesGained = (StartSlag:Stroke , slutSlag:Stroke, latitude:number , longitude:number) =>{
     var rvalue = calculateRawSG(StartSlag,latitude,longitude) - 1 - calculateRawSG(slutSlag,latitude,longitude)
     //console.log("Strokes Gained: ", rvalue)
     return rvalue
   }
-  
+
   const calculateRawSG = (slag:Stroke, latitude:number , longitude:number) => {
     if (!slag.lie){
       slag.lie = 1 //lie = 1 Equals "Fairway"
@@ -76,8 +77,8 @@ export
     const firstSGGreen = sGdata.green["Green"][indices[0]]
     const secondSGGreen = sGdata.green["Green"][indices[1]]
     const firstDistanceGreen = sGDistanceGreen[indices[0]]
-    const secondDistanceGreen = sGDistanceGreen[indices[1]] 
-    //TODO: 
+    const secondDistanceGreen = sGDistanceGreen[indices[1]]
+    //TODO:
     //Lägg till manuell inmatning där man pekar mot flaggan istället för GPS koordinater
 
 
@@ -100,3 +101,66 @@ export
     return [distanceArray.length - 2, distanceArray.length - 1]; // fallback
   }
 
+  export const calculateAverage = (array: number[]): number => {
+    const sum = array.reduce((a: number, b: number): number => a + b);
+    return sum / array.length;
+};
+
+export const generateStatTables = async (): strokes[][]=>{
+    let strokeTables= [1,2,3];
+    const strokes = await db.getStrokes();
+    //console.log('All strokes: ', strokes);
+    for (const stroke of strokes) {
+        console.log('Stroke: ', stroke.roundId);
+        let myRound = await db.getRoundById(stroke.roundId);
+        console.log('myRound:' , myRound.courseId);
+        const hole = getHole(myRound.courseId, stroke.holeId)
+        console.log('hole: ', hole);
+        //const dbhole = async db.getHole
+        const distance = calculateDistance(stroke.startLatitude, stroke.startLongitude, hole.greenMiddle.latitude, hole.greenMiddle.longitude );
+        console.log('distance: ', distance);
+    }
+     /*db.getStrokes()
+       .then((strokes) => {
+       console.log('All strokes: ', strokes);
+       strokes.forEach((stroke, index) => {
+               console.log('Stroke: ', stroke);
+               var myround;
+               db.getRoundById(stroke.roundId).then((round) => {
+                   console.log('round:' , round);
+                   myround = round;
+                   console.log('myround:' , myround);
+                      console.log('Getting Hole from course Id:',round.courseId,' and hole:', stroke.holeId);
+                      const hole = getHole(round.courseId, stroke.holeId);
+                      console.log('hole: ', hole)
+                      const distance = calculateDistance(stroke.startLatitude, stroke.startLongitude, hole.latitude, hole.longitude );
+
+                      if (stroke.golfClubId == 0){
+                          strokeTables[3].append(stroke);
+                      } // Club 0 will always be putter.
+
+                      else if (distance < 50){
+                          strokeTables[2].append(stroke);
+                      }
+                      else if (stroke.lie = 0 && hole.par >= 4){
+                          strokeTables[0].append(stroke);
+                      }
+                      else{
+                          strokeTables[1].append(stroke);
+                      }
+               })
+               //const aaaa = db.getRoundById(stroke.roundId);
+
+
+             });
+           //strokeTables[1] = strokes; //ALL strokes are put in table as approach.
+
+       })
+       .catch((error) => {
+       console.error('Error fetching strokes:', error);
+       });
+*/
+
+    return strokeTables
+
+};
