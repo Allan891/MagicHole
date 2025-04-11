383e47c64ee7bf39dba37b238cdfed779d7b97c6
MEK <mekmmeekk@proton.me>
2025-04-11 09:13:56 +0200

    Fixed getHoleById and cleaned up settings in db

--- app/db/db.ts
+++ app/db/db.ts
@@ -139,38 +139,8 @@ class Database {
     }
   }
 
-    // Settings
-    async setSetting(setting: string, value: string) {
-      if (!this.db) return;
-      try {
-        await this.db.runAsync(
-          'REPLACE INTO Settings (setting, value) VALUES (?, ?);',
-          setting, value);
-        console.log('Setting updated');
-      } catch (error) {
-        console.error('Error updating setting:', error);
-      }
-    }
-
-    async getSettings(): Promise<[]> {
-      if (!this.db) return [];
-      try {
-        const settings = await this.db.getAllAsync(
-          'SELECT * FROM Settings;');
-          const output = settings.reduce((acc, { setting, value }) => {
-            acc[setting] = value
-            return acc;
-          }, {});          
-          return output;
-        
-      } catch (error) {
-        console.error('Error getting settings:', error);
-        return [];
-      }
-    }
-
-  // DELETE: Delete a course
-  async deleteCourse(course: Course) {
+   // DELETE: Delete a course
+   async deleteCourse(course: Course) {
     if (!this.db) return;
     try {
       await this.db.runAsync('DELETE FROM Course WHERE id = ?;', course.id);
@@ -204,6 +174,40 @@ class Database {
 
 //#endregion
 
+
+
+//#region CRUD Operations for Settings
+  // Settings
+  async setSetting(setting: string, value: string) {
+    if (!this.db) return;
+    try {
+      await this.db.runAsync(
+        'REPLACE INTO Settings (setting, value) VALUES (?, ?);',
+        setting, value);
+      console.log('Setting updated');
+    } catch (error) {
+      console.error('Error updating setting:', error);
+    }
+  }
+
+  async getSettings(): Promise<[]> {
+    if (!this.db) return [];
+    try {
+      const settings = await this.db.getAllAsync(
+        'SELECT * FROM Settings;');
+        const output = settings.reduce((acc, { setting, value }) => {
+          acc[setting] = value
+          return acc;
+        }, {});          
+        return output;
+      
+    } catch (error) {
+      console.error('Error getting settings:', error);
+      return [];
+    }
+  }
+//#endregion
+ 
 
 
 //#region CRUD Operations for Hole Table
@@ -267,7 +271,7 @@ class Database {
   async getHoleById(id: number): Promise<Hole | null> {
     if (!this.db) return null;
     try {
-      return await this.db.getFirstAsync<Hole>('SELECT * FROM Stroke WHERE id = ?;', id);
+      return await this.db.getFirstAsync<Hole>('SELECT * FROM Hole WHERE id = ?;', id);
     } catch (error) {
       console.error('Error fetching hole:', error);
       return null;
