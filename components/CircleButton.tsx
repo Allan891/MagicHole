8e5183eebf74bbcb859ebf4a43380882761bca04
f-r00t <fruktstav@gmail.com>
2025-04-11 11:57:38 +0200

    Update button

--- components/CircleButton.tsx
+++ components/CircleButton.tsx
@@ -2,7 +2,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { useEffect } from 'react';
 import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 
-export function CircleButton({onPress, icon, label}) {
+export function CircleButton({onPress, icon, label, disabled=false}) {
 
 
   useEffect(() => {
@@ -10,9 +10,9 @@ export function CircleButton({onPress, icon, label}) {
 
 
   return (
-    <View style={{ alignItems: 'center' }}>
+    <View style={{ alignItems: 'center', opacity: disabled ? 0.5 : 1 }}>
 
-    <TouchableOpacity style={styles.circleButton} onPress={onPress}>
+    <TouchableOpacity style={styles.circleButton} onPress={disabled ? ()=>{} : onPress }>
       <MaterialIcons name={icon} size={28} color="black" />
     </TouchableOpacity>
     {label &&
