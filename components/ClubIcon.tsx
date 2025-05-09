a1f0eb1b29be4e50e3d3095d75cd8c9b59513b51
MEK <mekmmeekk@proton.me>
2025-05-09 10:35:51 +0200

    Deafult bag updated

--- components/ClubIcon.tsx
+++ components/ClubIcon.tsx
@@ -31,7 +31,6 @@ const CLUBS = {
 
 export function ClubIcon({ item, label=true }) {
 
-  console.log('club type:', item.iconType);
   const club = CLUBS[item.iconType];
  
   if (!club) return null;
