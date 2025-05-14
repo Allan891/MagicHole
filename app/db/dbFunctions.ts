4588e1a354fa12004f555b76c8ad4310b6f5c283
MEK <mekmmeekk@proton.me>
2025-05-14 09:12:45 +0200

    Update dbFunctions.ts

--- app/db/dbFunctions.ts
+++ app/db/dbFunctions.ts
@@ -10,20 +10,32 @@ class FillDbdata{
         }
       
         for (const club of golfClubs) {
-          if (club.type == GolfClubType.putter || club.type == GolfClubType.driver ||
+          try {
+            let golfClub;
+            if (club.type == GolfClubType.putter || club.type == GolfClubType.driver ||
             club.type == GolfClubType.sWedge || club.type == GolfClubType.pWedge ||
             club.type == GolfClubType.iron9 || club.type == GolfClubType.iron8 || club.type == GolfClubType.iron7 ||
             club.type == GolfClubType.iron6 || club.type == GolfClubType.iron5 ||
-            club.type == GolfClubType.wood3)
-          try {
-            const golfClub = {
+            club.type == GolfClubType.wood3){
+               golfClub = {
                 id: club.id,
                 playerId: club.playerId,
                 name: club.name,
                 type: club.type,
                 showInList: club.showInList,
                 iconType: club.iconType
-            };
+              };
+            }
+            else{
+               golfClub = {
+                id: club.id,
+                playerId: club.playerId,
+                name: club.name,
+                type: club.type,
+                showInList: 0,
+                iconType: club.iconType
+              };
+            }
             const clubInDb = await db.getGolfClubById(golfClub.id);
             if (clubInDb == null){
                 await db.createGolfClub(golfClub);
